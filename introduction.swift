fe344b0e9a2e4fabcf588887bd5b1f5f710e70d6
imammasleap <imam@masleap.io>
2022-02-11 20:03:49 +0600

    feat: swift intro level code added

---
+++ introduction.swift
@@ -0,0 +1,30 @@
+print("Hello World")
+
+// This is swift single line comments
+
+/*
+    This is swift 
+    multiline 
+    comments
+*/
+
+print("Printing Hello World Again")
+
+
+// Swift Variables
+var learnerName = "imam"
+print(learnerName)
+
+// Decleare a variable in swift
+var name:String
+var age:Int
+var money:Float
+var radious:Double
+
+// We have to decleare 
+print(name)
+
+
+
+
+
