2fd3cb8965bc36359d553449ac04ed3244d79641
imammasleap <imam@masleap.io>
2022-02-14 17:56:20 +0600

    feat: swift operatrion lession completed

---
+++ 05_operation.swift
@@ -0,0 +1,46 @@
+// Swift arithmetic operation
+
+print(4+5)
+print(4-5)
+print(4*5)
+print(20/5)
+print(20.0/5.0)
+print(20 % 6)
+
+// Swift assignment operators
+
+var a:Float = 20.0;
+var b:Float = 5.0;
+
+a += 1 // ++ operator removed ion swift 3
+
+print(a)
+
+a = b
+
+print(a)
+
+print(a > b)
+
+let d:Any = a==b ?"Hello":"Bye" // Don't fopget to put extra space after condition
+
+
+print(d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
